8a2a07e459750edd4e2903ace1be096c2185b6aa
Christopher Dodd <christopherrdodd@gmail.com>
2022-03-17 14:12:55 +1100

    Create inner HTML - 3 ways

--- component.js
+++ component.js
@@ -1,7 +1,25 @@
 class WebComponent extends HTMLElement {
   constructor() {
     super();
-    console.log(this.innerHTML);
+
+    // 1. By simply setting the innerHTML
+    this.innerHTML = `
+      <div>
+        <span>This is a web component</span>
+      </div>
+    `;
+
+    // 2. By creating elements and add them to the DOM
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    span.innerHTML = "This is a web component";
+    div.appendChild(span);
+    this.appendChild(div);
+
+    // 3. Parse a HTML string
+    const html = `<div><span>This is a web component</span></div>`
+    const innerHTML = new DOMParser().parseFromString(html, 'text/html').body.innerHTML;
+    this.innerHTML = innerHTML;
   }
 }
 
