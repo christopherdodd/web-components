06f6355cde3eae66c8dd502b82d599eb6f0c46be
Christopher Dodd <christopherrdodd@gmail.com>
2022-03-23 11:47:43 +1100

    Lifecycle methods

--- component.js
+++ component.js
@@ -1,4 +1,7 @@
 class WebComponent extends HTMLElement {
+
+  // 1. Constructor - created in memory but not necessarily attached to the DOM yet
+  // Best place for initialisation, not the best place for interacting with the DOM
   constructor() {
     super();
 
@@ -6,23 +9,34 @@ class WebComponent extends HTMLElement {
       this.innerTextContent = this.getAttribute('text');
     }
 
-    // NOTE: Attributes are not the same as properties
-    // If I am to console.log this.text, it will come up undefined
-    console.log('this.text =', this.text); 
-
     this.innerHTML = `
       <div>
         <span>${this.innerTextContent}</span>
       </div>  
     `;
+
+    console.log(this.innerHTML);
+    console.log('constructor');
   }
 
+  // 2. Connected Callback - When the element is attached from the DOM
+  connectedCallback() {
+    console.log('connected callback');
+  }
+
+  // 3. Attribute Changed Callback - checking if an attribute has changed
   attributeChangedCallback(attrName, oldValue, newValue) {
     if(attrName == 'text') this.innerTextContent = this.innerHTML = `
       <div>
         <span>${newValue}</span>
       </div>
     `;
+  }
+
+  // 4. Disconnected Callback - When the element is dettached from the DOM
+  // Useful for performing cleanup
+  disconnectedCallback() {
+    console.log('web component has been removed');
   }
 
   static get observedAttributes() {
