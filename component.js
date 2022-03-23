2793c6d294799c1f7319ac424d24822ef1b7c9ef
Christopher Dodd <christopherrdodd@gmail.com>
2022-03-23 16:11:49 +1100

    Build a custom button

--- component.js
+++ component.js
@@ -1,48 +1,13 @@
-class WebComponent extends HTMLElement {
-
-  // 1. Constructor - created in memory but not necessarily attached to the DOM yet
-  // Best place for initialisation, not the best place for interacting with the DOM
+class CustomButton extends HTMLButtonElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: 'open' });
-
-    this.shadowRoot.innerHTML = `
-      <style>
-        :host {
-          font-weight: bold;
-        }
-        span {
-          /* background-color: black; */
-          background-color: var(--bg-color);
-          color: white;
-        }
-        ::slotted(span) {
-          /* background-color: black; */
-          background-color: var(--bg-color);
-        }
-      </style>
-      <div>
-        <div>
-          <slot name="slot-1"></slot>
-        </div>
-        <div>
-         <slot name="slot-2"></slot>
-        </div>
-      </div>  
-    `;
-
-    const slots = this.shadowRoot.querySelectorAll('slot');
-    // console.dir(slots[0]);
-    slots[0].addEventListener('slotchange', event => {
-      console.dir(slots[0]);
+    this.link = this.getAttribute('link');
+    this.addEventListener('click', event => {
+      event.preventDefault();
+      window.location.href = this.link;
     })
   }
-
-  disconnectedCallback() {
-    console.log('web component has been removed');
-  }
-
 }
 
-customElements.define('web-component', WebComponent);
+customElements.define('custom-button', CustomButton, { extends: 'button' });
